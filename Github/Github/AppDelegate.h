ef4b38579ffb0a8a683bf0b3e1cd800cd7763d4c
zhouyong <935675720@qq.com>
2016-01-14 10:12:59 +0800

    第一次提交版本1.0

---
+++ Github/Github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github
+//
+//  Created by zhouyong on 16/1/14.
+//  Copyright © 2016年 zy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Github/Github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github
+//
+//  Created by zhouyong on 16/1/14.
+//  Copyright © 2016年 zy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
